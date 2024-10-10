d2da0be05f53916b1e0098b05b0d6220ce4a5f4a
andresparceromelo <131316669+andresparceromelo@users.noreply.github.com>
2024-10-09 21:29:51 -0500

    Update Usuario.py
    se agrega autenticacion en abstraccion usuario

--- Trabajo codigo limpio/Usuario.py
+++ Trabajo codigo limpio/Usuario.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
 class Usuario(ABC):
-    def __init__(self, nombre: str)->None:
+    def __init__(self, nombre: str, password: any)->None:
         self.nombre:str = nombre
+
+    def autenticar(self, password: str) -> bool:
+        return self.password == password
